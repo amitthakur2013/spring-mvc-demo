fdf7264e057a870bbc0d90718dac065bbb5a72a5
amitthakur2013 <amitthakur2013@gmail.com>
2021-03-08 17:57:56 +0530

    custom validation

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -3,11 +3,15 @@ package com.luv2code.springdemo.mvc;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
 public class Customer {
 	private String firstName;
 	
 	@NotNull(message="is required")
-	@Size(min=1)
+	@Size(min=1,message="is required")
 	private String lastName;
 	public String getFirstName() {
 		return firstName;
@@ -21,4 +25,5 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 }
